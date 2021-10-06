8266e7ea9b3e4e505e60c84cea5508b17403a172
chelsea-spratley <cs3624@drexel.edu>
2021-10-06 11:40:55 -0400

    new

---
+++ dev/js/scripts.js
@@ -0,0 +1,6 @@
+import { gsap } from "gsap";
+
+const mainTL = gsap.timeline();
+
+mainTL.from("#hero article",{duration:3,alpha:0, scale:2, rotation:360});
+mainTL.from("#hero h1",{duration:3,alpha:0, scale:2});
