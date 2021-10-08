6fa79cd4118ace5413cb400af23a5ecfa205f997
chelsea-spratley <cs3624@drexel.edu>
2021-10-08 08:19:07 -0400

    js fix

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,3 +4,4 @@ const mainTL = gsap.timeline();
 
 mainTL.from("#hero article",{duration:3,alpha:0, scale:2, rotation:360});
 mainTL.from("#hero h1",{duration:3,alpha:0, scale:2});
+mainTL.from("#flower",{duration:4,alpha:0,scale:1,rotation:720});
