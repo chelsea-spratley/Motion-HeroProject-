3b182c699445a521929d102bac0893900a31b494
chelsea-spratley <cs3624@drexel.edu>
2021-10-24 20:30:44 -0400

    preloader

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,45 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero article",{duration:3,alpha:0, scale:2, rotation:360});
-mainTL.from("#hero h1",{duration:3,alpha:0, scale:2});
-mainTL.from("#flower",{duration:4,alpha:0,scale:1,rotation:720});
+mainTL.to("logo-1",{duration:3,y:"=600"});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mainTL.from("#hero article",{duration:3,alpha:0, scale:2, rotation:360});
+// mainTL.from("#hero h1",{duration:3,alpha:0, scale:2});
+// mainTL.from("#flower",{duration:4,alpha:0,scale:1,rotation:720});
