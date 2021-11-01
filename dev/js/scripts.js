850edad26f94d07b7b818dcb7e5b7aee51d45755
chelsea-spratley <cs3624@drexel.edu>
2021-10-31 21:53:41 -0400

    devtools

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,39 +1,13 @@
 import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.to("logo-1",{duration:3,y:"=600"});
+mainTL.to("logo-1",{duration:3, alpha:0});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gsap.registerPlugin(GSDevTools);
 
 
 
@@ -44,3 +18,4 @@ mainTL.to("logo-1",{duration:3,y:"=600"});
 // mainTL.from("#hero article",{duration:3,alpha:0, scale:2, rotation:360});
 // mainTL.from("#hero h1",{duration:3,alpha:0, scale:2});
 // mainTL.from("#flower",{duration:4,alpha:0,scale:1,rotation:720});
+GSDevTools.create();
