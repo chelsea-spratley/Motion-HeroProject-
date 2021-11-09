1c4f3a4f40a785ac782d07f890bcdd8060f50692
chelsea-spratley <cs3624@drexel.edu>
2021-11-09 11:04:18 -0500

    preloader

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,38 +1,48 @@
 import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
 
 const mainTL = gsap.timeline();
 
+MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
+
 function dropIn(){
-    const tl = gsap.timeline();
 }
+
+// function background(){
+//     tl.to("#background1", 0)
+// }
 
 function cookie(){
 
     const tl = gsap.timeline();
 
     tl.from("#logo-9", { duration:0.5, y:"-=100",  autoAlpha:0})
-    tl.from("#logo-10", { duration:0.5, y:"-=100",  autoAlpha:0})
-    tl.from("#logo-11", { duration:0.5, y:"-=100",  autoAlpha:0})
-    tl.from("#logo-12", { duration:0.5, y:"-=100",  autoAlpha:0})
+    .from("#logo-10", { duration:0.5, y:"-=100",  autoAlpha:0})
+    .from("#logo-11", { duration:0.5, y:"-=100",  autoAlpha:0})
+    .from("#logo-12", { duration:0.5, y:"-=100",  autoAlpha:0})
 
-    tl.from("#logo-5", { duration:0.5, y:"-=75",  autoAlpha:0})
-    tl.from("#logo-6", { duration:0.5, y:"-=75",  autoAlpha:0})
-    tl.from("#logo-7", { duration:0.5, y:"-=75",  autoAlpha:0})
-    tl.from("#logo-8", { duration:0.5, y:"-=75",  autoAlpha:0})
+    .from("#logo-5", { duration:0.5, y:"-=75",  autoAlpha:0})
+    .from("#logo-6", { duration:0.5, y:"-=75",  autoAlpha:0})
+    .from("#logo-7", { duration:0.5, y:"-=75",  autoAlpha:0, morphSVG:"#flower1"})
+    .from("#logo-8", { duration:0.5, y:"-=75",  autoAlpha:0})
 
-    tl.from("#logo-1", { duration:0.5, y:"-=50",  autoAlpha:0})
-    tl.from("#logo-2", { duration:0.5, y:"-=50",  autoAlpha:0})
-    tl.from("#logo-3", { duration:0.5, y:"-=50",  autoAlpha:0})
-    tl.from("#logo-4", { duration:0.5, y:"-=50",  autoAlpha:0})
+    .from("#logo-1", { duration:0.5, y:"-=50",  autoAlpha:0})
+    .from("#logo-2", { duration:0.5, y:"-=50",  autoAlpha:0})
+    .from("#logo-3", { duration:0.5, y:"-=50",  autoAlpha:0})
+    .from("#logo-4", { duration:0.5, y:"-=50",  autoAlpha:0})
+
+    .to("#preloader",{duration:0.25, alpha:0, onComplete:backToTop})
+
+    return tl;
 }
 
-// function backToTop(){
-//     window.scrollTo(0,0);
-//     gsap.set("#preloader",{display:"none"});
-// }
+function backToTop(){
+    window.scrollTo(0,0);
+    gsap.set("#preloader",{display:"none"});
+}
 
 function hero(){
 
@@ -42,10 +52,9 @@ tl.from("#hero article",{duration:3,alpha:0, scale:2, rotation:360});
 tl.from("#hero h1",{duration:3,alpha:0, scale:2});
 tl.from("#flower",{duration:4,alpha:0,scale:1,rotation:720});
 
-
+return tl;
 
 }
-
 
 mainTL.add(dropIn())
 .add(cookie())
